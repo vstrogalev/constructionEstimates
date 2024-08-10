5e5227ce9fee0a545f9336fb187cba59f7f6a529
Viacheslav Strogalev <strogalev@hotmail.com>
2024-08-10 20:05:54 +0300

    Fixed address to server

--- src/common/_constants.ts
+++ src/common/_constants.ts
@@ -1,3 +1,3 @@
 export const REACT_APP_ENTITY_ID_API = '135123'; // ID, полученный по API {{baseUrl}}/v1/outlay-rows/entity/create
-export const REACT_APP_CONSTRUCTION_COSTS_API_URL='http://185.244.172.108:8081/v1/outlay-rows/entity'
+export const REACT_APP_CONSTRUCTION_COSTS_API_URL='https://185.244.172.108:8081/v1/outlay-rows/entity'
 
